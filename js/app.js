bbc329ca7b68e8b50a59faf94558b4696706bbac
jmartymar <jeffjmart@gmail.com>
2022-07-28 12:12:08 -0500

    fix hand text inconsistencies, refactor

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
 /*----- constants -----*/
-/*----- app's state (variables) -----*/
-/*----- cached element references -----*/
-/*----- event listeners -----*/
-/*----- functions -----*/
-
-/*----- constants -----*/
 const cardSuits = ['clubs', 'diamonds', 'hearts', 'spades'];
 const cardValues = ['r02', 'r03', 'r04', 'r05', 'r06', 'r07', 'r08', 'r09', 'r10', 'J', 'Q', 'K', 'A'];
 const cards = buildCardsObj(); // build the cards obj which returns both the cardsObj and deckArr initial state
 const cardsObj = cards[0]; // initialize cardsObj - list of all card atributes
 const fullDeckArr = cards[1]; // initialize fullDeckArr - list of all card names in a deck
 
-
 /*----- app's state (variables) -----*/
-
 let deckArr = [...fullDeckArr];
 let handObj = [];
 let handArr = [];
@@ -42,6 +34,8 @@ const standButtonEls = {
     3: document.getElementById('stand4'),
     4: document.getElementById('stand5')
 }
+const betButtonEls = document.querySelectorAll('.bet-btns');
+
 const chipTotalEl = document.getElementById('chip-total');
 const chipPileEl = document.getElementById('chip-pile');
 const currentBetEl = document.getElementById('current-bet');
@@ -54,7 +48,6 @@ const playMusicButtonEl = document.getElementById('play-music-btn');
 const addFundsSaveButtonEl = document.getElementById('add-funds-save');
 const addFundsConfirmEl = document.getElementById('add-funds-alert');
 const addFundsButtonEl = document.getElementById('add-funds-button');
-const betButtonEls = document.querySelectorAll('.bet-btns');
 
 /*----- sprite constants -----*/
 const chipCanvasEl = document.getElementById('chip-canvas');
@@ -64,9 +57,45 @@ const spriteHeight = 72;
 const spriteCols = 5;
 const spriteRows = 2;
 const chipImages = new Image();
-chipImages.crossOrigin="anonymous";
-chipImages.src = 'https://i.imgur.com/PHUUJk0.png';
-console.log(chipImages,'<-chipImages');
+chipImages.crossOrigin='anonymous';
+chipImages.src = 'https://raw.githubusercontent.com/jmartymar/SEI-P1-Video-Poker/main/images/chips-spritesheet-128x72.png';
+const spriteMapObj = {
+    7: {
+        increment: 1000000,
+        chipSpriteX: 0,
+        chipSpriteY: 1
+    },
+    6: {
+        increment: 100000,
+        chipSpriteX: 1,
+        chipSpriteY: 1
+    },
+    5: {
+        increment: 10000,
+        chipSpriteX: 2,
+        chipSpriteY: 1
+    },
+    4: {
+        increment: 1000,
+        chipSpriteX: 4,
+        chipSpriteY: 0
+    },
+    3: {
+        increment: 100,
+        chipSpriteX: 2,
+        chipSpriteY: 0
+    },
+    2: {
+        increment: 10,
+        chipSpriteX: 1,
+        chipSpriteY: 0
+    },
+    1: {
+        increment: 1,
+        chipSpriteX: 3,
+        chipSpriteY: 0
+    }
+}
 
 /*----- audio constants ------*/
 const backgroundMusic = new Audio('audio/Kenny_Rogers_-_The_Gambler-Totally-Paid-For.mp3');
@@ -89,7 +118,6 @@ document.getElementById('bet-add').addEventListener('click', addBet);
 document.getElementById('bet-minus').addEventListener('click', minusBet);
 document.getElementById('bet-max').addEventListener('click', addMaxBet);
 document.getElementById('add-funds-save').addEventListener('click', addFunds);
-
 
 playMusicButtonEl.addEventListener('click', playMusic);
 volume.addEventListener('input', function(e) {
@@ -131,7 +159,6 @@ function playMusic() {
         playMusicButtonEl.classList.add('btn-danger');
         playMusicButtonEl.classList.remove('btn-warning');
     }
-
 }
 
 /*----- functions -----*/
@@ -175,12 +202,14 @@ function addBet() {
     }
     render();
 }
+
 function minusBet() {
     if(betAmount > 0) {
         betAmount -= 1;
     }
     render();
 }
+
 function addMaxBet() {
     if(betAmount < 5) {
         betAmount += (5 - betAmount);
@@ -196,9 +225,8 @@ function standCard(num) {
             standArr.push(num);
         }
     }
-render();
+    render();
 }
-
 
 function buildCardsObj() {
     let cardFace;
@@ -234,7 +262,7 @@ function buildHand(deckArr, cardsWantedArr = [0, 1, 2, 3, 4]) {
             currentHand[i] = currentHand[i];
         }
     }
-return currentHand;
+    return currentHand;
 }
 
 function playHand() {
@@ -245,8 +273,7 @@ function playHand() {
         newHand = false;
     } else {
         newHand = true;
-    }
-    
+    }    
     // if new game, build full hand, else update hand
     if(newHand) {
         init();
@@ -276,8 +303,7 @@ function playHand() {
     render();
 }
 
-
-/// https://www.tutorialspoint.com/convert-number-to-tens-hundreds-thousands-and-so-on-javascript
+// https://www.tutorialspoint.com/convert-number-to-tens-hundreds-thousands-and-so-on-javascript
 const placeValue = (num, res = [], factor = 1) => {
     if(num){
        const val = (num % 10) * factor;
@@ -287,9 +313,7 @@ const placeValue = (num, res = [], factor = 1) => {
     return res;
  };
 
-
 /*----- view functions -----*/
-
 function resetBoard() {
     for(let cardEl in cardEls) {
         if(!standArr.includes(+cardEl)) {
@@ -303,57 +327,24 @@ function getTile(x, y, chipX, chipY) {
     return chipCanvasEl.toDataURL('image/png');
 }
 
-
 function drawChips() {
     ctx.clearRect(0, 0, chipCanvasEl.width, chipCanvasEl.height);
     chipCanvasEl.innerHTML = '';
-    const spriteMapObj = {
-        7: {
-            increment: 1000000,
-            chipSpriteX: 0,
-            chipSpriteY: 1
-        },
-        6: {
-            increment: 100000,
-            chipSpriteX: 1,
-            chipSpriteY: 1
-        },
-        5: {
-            increment: 10000,
-            chipSpriteX: 2,
-            chipSpriteY: 1
-        },
-        4: {
-            increment: 1000,
-            chipSpriteX: 4,
-            chipSpriteY: 0
-        },
-        3: {
-            increment: 100,
-            chipSpriteX: 2,
-            chipSpriteY: 0
-        },
-        2: {
-            increment: 10,
-            chipSpriteX: 1,
-            chipSpriteY: 0
-        },
-        1: {
-            increment: 1,
-            chipSpriteX: 3,
-            chipSpriteY: 0
-        }
-    }
     if(chipTotal > 0) {     
         let chip;
         const chipNode = document.createElement('img');
         const chipTensArr = placeValue(chipTotal);
         let xStart = 0;
+        // build chips image from spritemap
         for(let value of chipTensArr) {
             let yStart = 120;
             let spriteObj = spriteMapObj[value.toString().length];
             for(i = 0; i < value; i += spriteObj.increment) {        
-                chipNode.src = getTile(spriteObj.chipSpriteX,spriteObj.chipSpriteY,xStart,yStart);
+                chipNode.src = getTile(
+                    spriteObj.chipSpriteX,
+                    spriteObj.chipSpriteY,
+                    xStart,
+                    yStart);
                 yStart -= 6;
             }
             xStart += 50;
@@ -363,61 +354,74 @@ function drawChips() {
 }
 
 function render() {
-    //update card images
+    // update card images
     const winningHand = getWinningHand(handArr);
     let betClass;
     if(!newGame) {
         let i = 1;
-
+        currentHandValueEl.querySelector('h1').innerText = '';
         for(let cardEl in cardEls) {
-            console.log(cardEl);
              setTimeout(function(){
                  cardEls[cardEl].querySelector('img').src = handArr[cardEl].imgUrl;
-                 //dealing
              }, 200 * i);
         i++;
         }
-        //update stand card html
+        // update stand card html
         for(const standButton in standButtonEls) {
             if(newHand) {
-                cardEls[+standButton].querySelector('img').style.border = standArr.includes(+standButton) ? '2px solid red' : 'none';
-                standButtonEls[+standButton].classList.remove(standArr.includes(+standButton) ? 'btn-danger' : 'btn-info');
-                standButtonEls[+standButton].classList.add(standArr.includes(+standButton) ? 'btn-info' : 'btn-danger');    
+                cardEls[+standButton].querySelector('img').style.border = 
+                standArr.includes(+standButton) ? '2px solid red' : 'none';
+                standButtonEls[+standButton].classList.remove(standArr.includes(+standButton) ?
+                    'btn-danger' : 
+                    'btn-info');
+                standButtonEls[+standButton].classList.add(standArr.includes(+standButton) ? 
+                    'btn-info' : 
+                    'btn-danger');    
                 standButtonEls[+standButton].disabled = false;    
             } else {
                 standButtonEls[+standButton].disabled = true;
 
             }
         }
-        document.querySelectorAll('.bet-row').forEach(function(el) { // reset all bet rows
+        // reset all bet rows
+        document.querySelectorAll('.bet-row').forEach(function(el) { 
             el.classList.remove('bg-danger');           
         })
-        
         if(winningHand) {
-            let handId =  winningHand[0].toLowerCase().split(" ").join('-');
+            let handId =  winningHand[0].toLowerCase().split(' ').join('-');
             document.getElementById(handId).classList.add('bg-danger');        
             setTimeout(function(){
-                currentHandValueEl.querySelector('h1').innerText = newHand ? "You have " + winningHand[0] : 'You won with ' + winningHand[0] + '!'; //update winning hand text
+                // update winning hand text
+                currentHandValueEl.querySelector('h1').innerText = newHand ? 
+                    'You have: ' + winningHand[0] : 
+                    'You won with: ' + winningHand[0] + '!'; 
             }, 1000);
         } else {
             setTimeout(function(){
-                currentHandValueEl.querySelector('h1').innerText = newHand ? 'Nothing yet' : "You're a LOOOOOOOSER"; //update losing hand text
+                //update losing hand text
+                currentHandValueEl.querySelector('h1').innerText = newHand ? 
+                'Nothing yet' : 
+                "You're a LOOOOOOOSER";
             }, 1000);            
         }
         for(i = 1; i <= 5; i++) {
             betClass = '.bet-' + i;
-            document.querySelectorAll(betClass).forEach(function(el) { // reset all bet columns
+            // reset all bet columns
+            document.querySelectorAll(betClass).forEach(function(el) { 
                 el.classList.remove('bg-danger');          
             })
             if(i === betAmount) {
-                document.querySelectorAll(betClass).forEach(function(el) { // update current bet column
+                // update current bet column
+                document.querySelectorAll(betClass).forEach(function(el) { 
                     el.classList.add('bg-danger');            
                 })
             }
         }
     }
     if(!newHand || newGame) {
-        betAmount <= 0 || chipTotal <= 0 ? dealButtonEl.disabled = true : dealButtonEl.disabled = false;
+        betAmount <= 0 || chipTotal <= 0 ? 
+            dealButtonEl.disabled = true : 
+            dealButtonEl.disabled = false;
     }
     if((newHand && newGame) || !newHand) {
         dealButtonEl.innerText = 'Deal Hand';
@@ -431,9 +435,9 @@ function render() {
         })
 
      }
-    newHand && !newGame ? currentHandValueEl.classList.remove('bg-primary') : currentHandValueEl.classList.add('bg-primary');
     
-    chipTotalEl.innerText = '$' + chipTotal.toLocaleString('en-US'); // update chip total text
+    // update chip total text
+    chipTotalEl.innerText = '$' + chipTotal.toLocaleString('en-US'); 
     document.getElementById('current-bet').innerText = 'Current bet: ' + betAmount;
     if(venmo) {
         venmoInputEl.value = '';
@@ -474,29 +478,23 @@ function render() {
             $('[data-bs-toggle="tooltip"]').tooltip('dispose');
         })
     }
-
 }
-
 
 /*----- hand test functions -----*/
 function isStraight(handArr) {
-
     let rankMin;
     let rankMax;
     let aceLowStraight = [0, 1, 2, 3, 12];
-    // handArr = [
-    //     {suit: 'diamonds', value: 'r02', imgUrl: 'images/diamonds/diamonds-r02.svg'},
-    //     {suit: 'diamonds', value: 'A', imgUrl: 'images/diamonds/diamonds-K.svg'},
-    //     {suit: 'diamonds', value: 'r07', imgUrl: 'images/diamonds/diamonds-r05.svg'},
-    //     {suit: 'spades', value: 'r04', imgUrl: 'images/spades/spades-J.svg'},
-    //     {suit: 'spades', value: 'r05', imgUrl: 'images/spades/spades-r06.svg'}
-    // ]
     const cardRanksArr = getCardRanksArr(handArr);
-    rankMin = Math.min(...cardRanksArr);   //get lowest card value
-    rankMax = Math.max(...cardRanksArr);    // get highest card value
-    if(cardRanksArr.every(value => (value >= rankMin) && (value <= rankMax) && (countDuplicates(cardRanksArr) === 0) && (rankMax - rankMin === 4) && handArr.length > 0)) { // if highest and lowest are within 4, not duplicates, and within the min/max range then it's a straight
+    rankMin = Math.min(...cardRanksArr);  //get lowest card value
+    rankMax = Math.max(...cardRanksArr);  // get highest card value
+    if(cardRanksArr.every(value => (value >= rankMin) && (value <= rankMax) && 
+        // if highest and lowest are within 4, not duplicates, 
+        //and within the min/max range then it's a straight
+        (countDuplicates(cardRanksArr) === 0) && (rankMax - rankMin === 4) && 
+        handArr.length > 0)) { 
         return true;
-    } else if(cardRanksArr.every(value => aceLowStraight.includes(value))) { // check if hand is ace low straight
+    } else if(cardRanksArr.every(value => aceLowStraight.includes(value))) {
         return true;
     } else {
         return false;
@@ -507,15 +505,13 @@ function isFlush(handArr) {
     const suitsArr = [];
     let suitMatches;
     for(const card of handArr) {
-        suitsArr.push(card.suit);        // create array of suites
+        suitsArr.push(card.suit);  // create array of suites
     }
     suitMatches = suitsArr.reduce((acc, value) => {  // reduce suits to get count of each suit
         acc[value] ? acc[value]++ : acc[value] = 1
         return acc;
     }, {});
-
-
-    if(Math.max(...Object.values(suitMatches)) === 5) { // check if any suits have a count of 5
+    if(Math.max(...Object.values(suitMatches)) === 5) {  // check if any suits have a count of 5
         return true;
     } else {
         return false;
@@ -545,10 +541,7 @@ function isPair(handArr) {
     for(const card of handArr) {          
         valueArr.push(card.value);
     }
-    valueMatches = valueArr.reduce((acc, value) => {
-        acc[value] ? acc[value]++ : acc[value] = 1
-        return acc;
-    }, {});
+    valueMatches = getValueMatches(valueArr);
     if(Object.values(valueMatches).some(value => value == 2)) {
         return true;
     } else {
@@ -562,10 +555,7 @@ function isFourOfKind(handArr) {
     for(const card of handArr) {          
         valueArr.push(card.value);
     }
-    valueMatches = valueArr.reduce((acc, value) => {
-        acc[value] ? acc[value]++ : acc[value] = 1
-        return acc;
-    }, {});
+    valueMatches = getValueMatches(valueArr);
     if(Object.values(valueMatches).some(value => value == 4)) {
         return true;
     } else {
@@ -580,11 +570,7 @@ function isTwoPair(handArr) {
     for(const card of handArr) {          
         valueArr.push(card.value);
     }
-    valueMatches = valueArr.reduce((acc, value) => {
-        acc[value] ? acc[value]++ : acc[value] = 1
-        return acc;
-    }, {});
-
+    valueMatches = getValueMatches(valueArr);
     for(const key in valueMatches) {
         if(valueMatches[key] === 2) {
             pairs++
@@ -596,7 +582,6 @@ function isTwoPair(handArr) {
         return false;
     }  
 }
-
 
 function countJackPlus(handArr) {
     let jacksCount = 0;
@@ -627,13 +612,10 @@ function isPairJacksPlus(handArr) {
     for(const card of handArr) {          
         valueArr.push(card.value);
     }
-    valueMatches = valueArr.reduce((acc, value) => {
-        acc[value] ? acc[value]++ : acc[value] = 1
-        return acc;
-    }, {});
-
+    valueMatches = getValueMatches(valueArr);
     for(const cardValue in valueMatches) {
-        if(+Object.keys(cardValues).find(key => cardValues[key] === cardValue) >= 9 && valueMatches[cardValue] == 2) {
+        if(+Object.keys(cardValues).find(key => 
+            cardValues[key] === cardValue) >= 9 && valueMatches[cardValue] == 2) {
             return true;
         }
     } 
@@ -740,11 +722,12 @@ function getWinningHand(handArr) {
 }
 
 /*----- utility functions -----*/
-
 function getCardRanksArr(handArr) {
     const cardRanksArr = [];
     for(const elem of handArr) {
-        cardRanksArr.push(+Object.keys(cardValues).find(key => cardValues[key] === elem.value)); // compare card to card values array to determine rank
+        // compare card to card values array to determine rank
+        cardRanksArr.push(+Object.keys(cardValues).find(key => 
+            cardValues[key] === elem.value)); 
     }
     return cardRanksArr;
 }
@@ -755,9 +738,13 @@ function countDuplicates(arr) {
     for(const item of arr) {
       countsByItem[item] = (countsByItem[item] || 0) + 1;
     }
-    return Object.values(countsByItem)
-      .filter(val => val >= 2)
-      .length;
+    return Object.values(countsByItem).filter(val => val >= 2).length;
+}
+function getValueMatches(arr) {
+    return arr.reduce((acc, value) => {
+        acc[value] ? acc[value]++ : acc[value] = 1
+        return acc;
+    }, {});
 }
 
 
